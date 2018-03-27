2466aa32cb0e01a79fea72d4e7f950fb238b4f23
itingkong <luoqi20131001@163.com>
2018-03-27 15:29:14 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.handleOnKeyUp = this.handleOnKeyUp.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+        value: '',
+        arr: [],
+        // text:'',
+    };
+  }
+
+  handleChange(e){
+      this.setState({value: e.target.value});
+  }
+
+  handleOnKeyUp(e){
+    if(e.keyCode === 13){
+        // this.setState({text: e.target.value});
+
+        this.setState({
+            arr: this.state.arr.concat({
+                text: this.state.value
+            }),
+            value:'',
+        });
+    }
+
+    console.log(this.state.value);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+          <input value={this.state.value}
+            onKeyUp={this.handleOnKeyUp}
+            onChange={this.handleChange}/>
+          <ul className="App-ul">
+              {
+                  this.state.arr.map((item, index) => {
+                      return(
+                          <li key={index} className='App-li'>{item.text}</li>
+                      )
+                  })
+              }
+          </ul>
+          {/*<p>{this.state.text}</p>*/}
+      </div>
+    );
+  }
+}
+
+export default App;
